9dcbfb506cdb7c495bed0034901771f35f21545d
jgttech <jgt.tech@protonmail.com>
2021-03-04 19:40:11 -0600

    Remove AWS & MongoDB creds/uri's. Re-establish Connections. Sanitize code.

--- packages/api/src/hooks/useAxiosCars.js
+++ packages/api/src/hooks/useAxiosCars.js
@@ -1,8 +1,5 @@
 import axios from "axios";
-
-const baseURL = location.hostname === "localhost"
-    ? process.env.API_HOST_LOCAL
-    : process.env.API_HOST;
+const baseURL = process.env.API_HOST;
 
 export const useAxiosCars = () => axios.create({
     validateStatus: status => status < 500,
